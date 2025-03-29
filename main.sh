562df8d728a90f8a683a44d8c9f873fdb8d71362
HunDavin <davin.hun@student.cadt.edu.kh>
2025-03-29 17:53:39 +0700

    update option in main

--- main.sh
+++ main.sh
@@ -2,16 +2,37 @@
 
 while true;do
     	echo
-	echo "1. Say hello"
-	echo "q to exit"
+	echo "|-----------------------------|"
+	echo "|			Bash Program		|"
+	echo "|-----------------------------|"
+	echo "|1. List file					|"
+	echo "|2. Create backup				|"
+	echo "|3. Count file				|"
+	echo "|4. Display disk usage		|"
+	echo "|5. Search file by name		|"
+	echo "|6. Compress file				|"
+	echo "|q. to exit					|"
+	echo "|-----------------------------|"
 	read -n 1 -s -p "Enter your choice" choice	
 
 	case $choice in
 		1)
-		
+		./script/listfile.sh
 		;;
 		2)
-		
+		./script/makebackup.sh
+		;;
+		3)
+		./script/countfile.sh
+		;;
+		4)
+		./script/displaydisk.sh
+		;;
+		5)
+		./script/search.sh
+		;;
+		6)
+		./script/compress.sh
 		;;
 		q)
 		
